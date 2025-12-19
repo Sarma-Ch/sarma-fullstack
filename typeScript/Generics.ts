d8dfae687dab7c19973582f7c36266beaf6bb665
NARASIMHA SARMA <nsarma2005@gmail.com>
2025-12-20 00:21:20 +0530

    update Generics

--- typeScript/Generics.ts
+++ typeScript/Generics.ts
@@ -57,6 +57,7 @@ console.log(arrLen);
 // const user = { id: 2, name: "Spill" };
 // console.log(lengthIs(user));
 
+// 4 Default Type Parameters
 type ApiResponse<T = string> = { status: number; data: T };
 
 const Api: ApiResponse = {
@@ -71,4 +72,99 @@ const Api1: ApiResponse1 = {
   data: "THis is not yet done",
 };
 console.log(Api1.status);
-console.log(Api1.data.toUpperCase("sarma"));
+// console.log(Api1.data.toUpperCase("sarma"));
+
+type Keys<T> = keyof T;
+
+interface User {
+  id: string;
+  age: number;
+}
+
+type UserKeys = Keys<User>;
+
+function getProp<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+const user1: User = { id: "one", age: 20 };
+
+const id = getProp(user1, "id");
+
+// const missing = getProp(user1, "missing")
+
+type user3 = {
+  id: number;
+  name: string;
+};
+
+const user3 = { id: 3, name: "Narasimha" };
+
+type Readonly<T> = { readonly [K in keyof T]: T[K] };
+type ReadonlyUser3 = Readonly<user3>;
+
+type Partial<T> = { [K in keyof T]?: T[K] };
+type Partial1 = Partial<user3>;
+const PartialUser: Partial1 = { name: "no" };
+console.log(PartialUser.id);
+console.log(PartialUser.name);
+
+// Test with IsString<string> and IsString<number>
+
+// type IsString<T> = T extends string ? true : false;
+// type IsStringName = IsString<user3["name"]>;
+// function getString<T extends user3>(user: T): T["name"] {
+//   return user.name;
+// }
+// console.log(getString(user3));
+
+// type IsStringNumber = IsString<user3["id"]>;
+// function GetString<T extends user3>(user: T): T["id"] {
+//   return user.id;
+// }
+// console.log(GetString(user3));
+
+// Conditional type to check if T is a string
+type IsString<T> = T extends string ? true : false;
+
+type TestString = IsString<string>;
+type TestNumber = IsString<number>;
+
+const check: IsString<string> = true;
+console.log("IsString<string>", check);
+const check1: IsString<number> = false;
+
+type ElementType<T> = T extends (infer U)[] ? U : T;
+
+type Test3 = ElementType<string[]>;
+type Test4 = ElementType<number[]>;
+type Test5 = ElementType<boolean>;
+
+//8
+
+interface user {
+  id: string;
+  age: number;
+}
+
+const userDict: Record<string, user> = {
+  first: { id: "A1", age: 18 },
+  second: { id: "B1", age: 22 },
+};
+console.log(userDict["first"]);
+
+type pickUserId = Pick<User, "id">;
+const userId: pickUserId = {
+  id: "one",
+};
+console.log("Picked id:", userId.id);
+
+type OmitAge = Omit<user, "age">;
+const forbidden: OmitAge = {
+  id: "two",
+};
+console.log("User ommited age ", forbidden.id);
+
+// function pluck<T, K extends keyof T>(objs: T[], key: K): T[K][]{
+//   return 
+// }
